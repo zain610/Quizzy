34b0fcf64f245f21b9a69cc6ec922ab10454d2a1
zain610 <znshroff@gmail.com>
2020-09-19 08:50:19 +1000

    Updates

---
+++ precache-manifest.fe768a437dccf469f8e9dfdfb8a70c39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682b847eaf377ec3435fbeb594d0f40e",
+    "url": "/quizzy/index.html"
+  },
+  {
+    "revision": "6e6c0b7e5b3e8c0fa406",
+    "url": "/quizzy/static/css/main.28e9ff86.chunk.css"
+  },
+  {
+    "revision": "babe876182e267eb422b",
+    "url": "/quizzy/static/js/2.68ab0e23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizzy/static/js/2.68ab0e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6c0b7e5b3e8c0fa406",
+    "url": "/quizzy/static/js/main.fa1543a5.chunk.js"
+  },
+  {
+    "revision": "a5dd9c7160e143953d27",
+    "url": "/quizzy/static/js/runtime-main.17a0daf1.js"
+  },
+  {
+    "revision": "10f0ef19cd4a2c157181601d61a4cb90",
+    "url": "/quizzy/static/media/quizzy-background.10f0ef19.jpg"
+  }
+]);
